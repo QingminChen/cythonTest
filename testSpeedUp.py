959598f41a3b5441fd830bff6d7f3c3440de656e
Chen, Qingmin <qingmin.chen@accenture.com>
2019-04-07 21:12:12 +0800

    before run cython code,pls make sure the string in the list are encoded with utf-8,otherwise it will throw out the errors,if change the code in the speedupInterfaces.pyx line #24 from 'ret[i] = PyString_AsString(list_str[i])' to 'ret[i] = PyString_AsString(list_str[i].encode('utf-8'))',it can't read the each of the string element from list correctly

--- testSpeedUp.py
+++ testSpeedUp.py
@@ -2,9 +2,17 @@ import setuptools
 import pyximport
 pyximport.install()
 import speedupInterfaces
-a=['qingmin','wenting','zhangsan','lisi']
-b=['qingmin','cloud','sky','march','wenting','happy','cry','shoes','zhangsan','mouse','lisi']
-c = speedupInterfaces.iterateFeaturesToFindIndicesInVocabulary(b,a)
+a=[u'qingmin',u'wenting',u'zhangsan',u'lisi']
+a_encode=[]
+for i in a:
+    a_i = i.encode('utf-8')
+    a_encode.append(a_i)
+b=[u'qingmin',u'cloud',u'sky',u'march',u'wenting',u'happy',u'cry',u'shoes',u'zhangsan',u'mouse',u'lisi']
+b_encode=[]
+for i in b:
+    b_i = i.encode('utf-8')
+    b_encode.append(b_i)
+c = speedupInterfaces.iterateFeaturesToFindIndicesInVocabulary(b_encode,a_encode)
 list =[]
 for i in c:
     list.append(i)
